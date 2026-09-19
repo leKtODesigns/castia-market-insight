2eea668e0eec6416f7ba27884dda56409643a14d
leKtO_ <lektodesigns@gmail.com>
2026-09-19 08:50:28 +0000

    Deploy: fdb1a6b

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 //     image hosts): pass through unchanged. The app is useless
 //     without network for live prices, so we don't pretend to
 //     support offline browsing of dynamic data.
-const VERSION = "castia-4c72441";
+const VERSION = "castia-fdb1a6b";
 const STATIC_CACHE = `${VERSION}-static`;
 const PRECACHE_URLS = [
   "./",
