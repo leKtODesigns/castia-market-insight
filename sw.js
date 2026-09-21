73d106454cdc45a3cc89e913f723dd5b31e6b872
leKtO_ <lektodesigns@gmail.com>
2026-09-21 00:35:58 +0000

    Deploy: 7c959e0

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 //     image hosts): pass through unchanged. The app is useless
 //     without network for live prices, so we don't pretend to
 //     support offline browsing of dynamic data.
-const VERSION = "castia-668436d";
+const VERSION = "castia-7c959e0";
 const STATIC_CACHE = `${VERSION}-static`;
 const PRECACHE_URLS = [
   "./",
